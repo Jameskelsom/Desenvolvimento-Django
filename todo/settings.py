c05cc2b6d207f11e4da533522beb881bcf623e0e
James Kelson <kelsomfire@hotmail.com>
2020-01-08 17:47:10 -0300

    index commit

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #aplicações
+    'todo.core',
 ]
 
 MIDDLEWARE_CLASSES = [
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from todo.core import views
 
 urlpatterns = [
+    url(r'', views.home),
     url(r'^admin/', admin.site.urls),
 ]
