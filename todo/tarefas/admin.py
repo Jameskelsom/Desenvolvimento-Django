5ac395a2cfa7aa1c545516549b45a9ca8781fbb6
James Kelson <kelsomfire@hotmail.com>
2020-01-09 18:52:48 -0300

    classe tarefas

--- todo/tarefas/admin.py
+++ todo/tarefas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Categoria, Tarefa
 # Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+class TarefaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Categoria)
+admin.site.register(Tarefa)
--- todo/tarefas/models.py
+++ todo/tarefas/models.py
@@ -4,3 +4,15 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
     descricao = models.TextField(verbose_name='Descrição')
+
+class Tarefa(models.Model):
+    PRIORIDADE_CHOICES = (
+        ('B', 'Baixa'),
+        ('M', 'Média'),
+        ('A', 'Alta'),
+    )
+    nome = models.CharField(u'Nome', max_length=100)
+    descricao = models.TextField(u'Descrição', blank=True)
+    data_final = models.DateField(u'Data final')
+    prioridade = models.CharField(u'Prioridade', max_length=1, choices=PRIORIDADE_CHOICES)
+    categoria = models.ForeignKey(Categoria, verbose_name='Categoria')
