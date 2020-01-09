95a906042d958b76dd879671390089b4b53daac8
James Kelson <kelsomfire@hotmail.com>
2020-01-09 16:00:07 -0300

    migrate

--- todo/tarefas/models.py
+++ todo/tarefas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    descricao = models.TextField(verbose_name='Descrição')
